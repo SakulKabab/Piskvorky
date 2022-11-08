7e678598a16cb01b67b0443b8c5d881bb11b3f68
Sakul Kabab <babakl.03@spst.eu>
2022-11-08 22:42:55 +0100

    Přidejte soubory projektu.

---
+++ Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿
+namespace Piškvorky
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.A1 = new System.Windows.Forms.Button();
+            this.A2 = new System.Windows.Forms.Button();
+            this.A3 = new System.Windows.Forms.Button();
+            this.B1 = new System.Windows.Forms.Button();
+            this.B2 = new System.Windows.Forms.Button();
+            this.B3 = new System.Windows.Forms.Button();
+            this.C1 = new System.Windows.Forms.Button();
+            this.C2 = new System.Windows.Forms.Button();
+            this.C3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // A1
+            // 
+            this.A1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.A1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.A1.Font = new System.Drawing.Font("Segoe UI", 42F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.A1.Location = new System.Drawing.Point(12, 12);
+            this.A1.Name = "A1";
+            this.A1.Size = new System.Drawing.Size(85, 85);
+            this.A1.TabIndex = 0;
+            this.A1.TabStop = false;
+            this.A1.UseVisualStyleBackColor = false;
+            this.A1.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // A2
+            // 
+            this.A2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.A2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.A2.Font = new System.Drawing.Font("Segoe UI", 42F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.A2.Location = new System.Drawing.Point(96, 12);
+            this.A2.Name = "A2";
+            this.A2.Size = new System.Drawing.Size(85, 85);
+            this.A2.TabIndex = 1;
+            this.A2.TabStop = false;
+            this.A2.UseVisualStyleBackColor = false;
+            this.A2.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // A3
+            // 
+            this.A3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.A3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.A3.Font = new System.Drawing.Font("Segoe UI", 42F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.A3.Location = new System.Drawing.Point(180, 12);
+            this.A3.Name = "A3";
+            this.A3.Size = new System.Drawing.Size(85, 85);
+            this.A3.TabIndex = 2;
+            this.A3.TabStop = false;
+            this.A3.UseVisualStyleBackColor = false;
+            this.A3.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // B1
+            // 
+            this.B1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.B1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.B1.Font = new System.Drawing.Font("Segoe UI", 42F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.B1.Location = new System.Drawing.Point(12, 96);
+            this.B1.Name = "B1";
+            this.B1.Size = new System.Drawing.Size(85, 85);
+            this.B1.TabIndex = 3;
+            this.B1.TabStop = false;
+            this.B1.UseVisualStyleBackColor = false;
+            this.B1.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // B2
+            // 
+            this.B2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.B2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.B2.Font = new System.Drawing.Font("Segoe UI", 42F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.B2.Location = new System.Drawing.Point(96, 96);
+            this.B2.Name = "B2";
+            this.B2.Size = new System.Drawing.Size(85, 85);
+            this.B2.TabIndex = 4;
+            this.B2.TabStop = false;
+            this.B2.UseVisualStyleBackColor = false;
+            this.B2.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // B3
+            // 
+            this.B3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.B3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.B3.Font = new System.Drawing.Font("Segoe UI", 42F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.B3.Location = new System.Drawing.Point(180, 96);
+            this.B3.Name = "B3";
+            this.B3.Size = new System.Drawing.Size(85, 85);
+            this.B3.TabIndex = 5;
+            this.B3.TabStop = false;
+            this.B3.UseVisualStyleBackColor = false;
+            this.B3.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // C1
+            // 
+            this.C1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.C1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.C1.Font = new System.Drawing.Font("Segoe UI", 42F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.C1.Location = new System.Drawing.Point(12, 180);
+            this.C1.Name = "C1";
+            this.C1.Size = new System.Drawing.Size(85, 85);
+            this.C1.TabIndex = 6;
+            this.C1.TabStop = false;
+            this.C1.UseVisualStyleBackColor = false;
+            this.C1.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // C2
+            // 
+            this.C2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.C2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.C2.Font = new System.Drawing.Font("Segoe UI", 42F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.C2.Location = new System.Drawing.Point(96, 180);
+            this.C2.Name = "C2";
+            this.C2.Size = new System.Drawing.Size(85, 85);
+            this.C2.TabIndex = 7;
+            this.C2.TabStop = false;
+            this.C2.UseVisualStyleBackColor = false;
+            this.C2.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // C3
+            // 
+            this.C3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.C3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.C3.Font = new System.Drawing.Font("Segoe UI", 42F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.C3.Location = new System.Drawing.Point(180, 180);
+            this.C3.Name = "C3";
+            this.C3.Size = new System.Drawing.Size(85, 85);
+            this.C3.TabIndex = 8;
+            this.C3.TabStop = false;
+            this.C3.UseVisualStyleBackColor = false;
+            this.C3.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(278, 277);
+            this.Controls.Add(this.C3);
+            this.Controls.Add(this.C2);
+            this.Controls.Add(this.C1);
+            this.Controls.Add(this.B3);
+            this.Controls.Add(this.B2);
+            this.Controls.Add(this.B1);
+            this.Controls.Add(this.A3);
+            this.Controls.Add(this.A2);
+            this.Controls.Add(this.A1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Piškvorky";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button A1;
+        private System.Windows.Forms.Button A2;
+        private System.Windows.Forms.Button A3;
+        private System.Windows.Forms.Button B1;
+        private System.Windows.Forms.Button B2;
+        private System.Windows.Forms.Button B3;
+        private System.Windows.Forms.Button C1;
+        private System.Windows.Forms.Button C2;
+        private System.Windows.Forms.Button C3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Piškvorky
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool hracNaRade = true;
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            if (hracNaRade)
+            {
+                button.Text = "X";
+                button.Enabled = false;
+            }
+            else
+            {
+                button.Text = "O";
+                button.Enabled = false;
+            }
+            hracNaRade = !hracNaRade;
+        }
+    }
+}
