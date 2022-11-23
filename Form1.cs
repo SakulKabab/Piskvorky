58bae680892f3791626de82f2b1a5a6dbf94d4d8
SakulKabab <100408195+SakulKabab@users.noreply.github.com>
2022-11-23 11:30:07 +0100

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -18,7 +18,34 @@ namespace Piškvorky
         }
 
         bool hracNaRade = true;
-
+        bool konecHry = false;
+        char[] pole = new pole[3,3];
+        
+        private void Tah(int radek, int sloupec)
+        {
+            if(!konecHry)
+            {
+                if(hracNaRade)
+                {
+                    pole[sloupec,radek]="X";
+                }
+                else
+                {
+                    pole[sloupec,radek]="O";
+                }
+                if(pole[0,0]==pole[1,0]&&pole[1,0]==pole[2,0])konecHry=true;
+                if(pole[0,1]==pole[1,1]&&pole[1,1]==pole[2,1])konecHry=true;
+                if(pole[0,2]==pole[1,2]&&pole[1,2]==pole[2,2])konecHry=true;
+                
+                if(pole[0,0]==pole[0,1]&&pole[0,1]==pole[0,2])konecHry=true;
+                if(pole[1,0]==pole[1,1]&&pole[1,1]==pole[1,2])konecHry=true;
+                if(pole[2,0]==pole[2,1]&&pole[2,1]==pole[2,2])konecHry=true;
+                
+                if(pole[0,0]==pole[1,1]&&pole[1,1]==pole[2,2])konecHry=true;
+                if(pole[0,2]==pole[1,1]&&pole[1,1]==pole[2,0])konecHry=true;
+            }
+        }
+/*
         private void Button_Click(object sender, EventArgs e)
         {
             Button button = (Button)sender;
@@ -34,5 +61,6 @@ namespace Piškvorky
             }
             hracNaRade = !hracNaRade;
         }
+        */
     }
 }
